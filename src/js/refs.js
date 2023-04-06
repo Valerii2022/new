a51e5875b2b3d617fa4069ec6ece41ba2f5491f7
Valerii Pometun <vmpometun@gmail.com>
2023-04-06 14:16:51 +0300

    small corrections

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export default refs = {
+  openMenuBtn: document.querySelector('[data-menu-open]'),
+  closeMenuBtn: document.querySelector('[data-menu-close]'),
+  menu: document.querySelector('[data-menu]'),
+  body: document.querySelector('body'),
+  menuList: document.querySelector('.menu-list'),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
